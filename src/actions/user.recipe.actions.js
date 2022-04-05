f5467b58e49024744ed98c89a32d3ccda5382450
Tait Digital <taitdigital@Taits-MBP.fritz.box>
2022-04-05 23:56:06 +1200

    wip

--- src/actions/user.recipe.actions.js
+++ src/actions/user.recipe.actions.js
@@ -1,36 +1,18 @@
-import * as RecipeService from '../services/recipe.service'
+import * as UserRecipeService from '../services/recipe.service'
 
-export const getRecipes = () => (dispatch) => {
-    return RecipeService.fetchRecipes().then((data) => {
+export const getUserRecipes = () => (dispatch) => {
+    return RecipeService.fetchUserRecipes().then((data) => {
         dispatch({
-          type: 'FETCH_RECIPES_SUCCESS',
-          payload: { recipes: data },
+          type: 'FETCH_USER_RECIPES_SUCCESS',
+          payload: { user_recipes: data },
         });
         return Promise.resolve()
       },
       (error) => {
         dispatch({
-          type: 'FETCH_RECIPES_FAIL',
+          type: 'FETCH_USER_RECIPES_FAIL',
           message: error
         });
-      
-        return Promise.reject()
-      })
-}
-
-export const getRecipe = (id) => (dispatch) => {
-    return RecipeService.fetchRecipe(id).then((data) => {
-        dispatch({
-          type: 'FETCH_RECIPE_SUCCESS',
-          payload: { current_recipe: data },
-        });
-        return Promise.resolve()
-      },
-      (error) => {
-        dispatch({
-          type: 'FETCH_RECIPE_FAIL',
-          message: error
-        })
       
         return Promise.reject()
       })
